a7998458da2bd4c92876e5891096004eb6aa877a
gourav0514 <gourav.m@rumango.com>
2025-04-27 11:16:05 +0530

    Modified: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AboutPage from "./components/About";
 import Navbar from "./components/Navbar";
 import ExperiencePage from "./components/Experience";
 import Skills from "./components/Skills";
+import ContactPage from "./components/Contact";
 
 const App = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -27,7 +28,7 @@ const App = () => {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/experience" element={<ExperiencePage />} />
         <Route path="/skills" element={<Skills />} />
-        <Route path="/contact" element={null} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </Router>
   );
